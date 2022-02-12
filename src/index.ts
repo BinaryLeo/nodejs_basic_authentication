9b3385863430674fafdb60401b1a4e58adee8a51
Binaryleo <leomouracss@fastmail.com>
2022-02-12 20:43:02 -0300

    initial files

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response, NextFunction } from 'express'
+
+const app = express()
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ foo: 'bar' })
+})
+app.listen(3000, () => {
+  console.log('App running on port 3000')
+})
