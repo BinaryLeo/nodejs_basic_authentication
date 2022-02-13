c2b28a659bc1138d93e750136d4ef828e1b2b936
Binaryleo <leomouracss@fastmail.com>
2022-02-12 22:24:51 -0300

    add http-status-codes

--- src/routes/users_route.ts
+++ src/routes/users_route.ts
@@ -1,15 +1,15 @@
 import {Router, Request, Response, NextFunction} from 'express';
-
+import {StatusCodes} from 'http-status-codes'
 const usersRoute = Router();
 
 usersRoute.get('/users',(req: Request, res: Response, next: NextFunction) => {
 const users = [{username: 'BinaryLeo'}];
-res.status(200).json({users});
+res.status(StatusCodes.OK).json({users});
 })
 
 usersRoute.get('/users/:uuid',(req: Request<{uuid:string}>, res: Response, next: NextFunction) => {
 const uuid = req.params.uuid;
-res.status(200).send({uuid});
+res.status(StatusCodes.OK).send({uuid});
 });
 
 export default usersRoute;
