e1a6114e08d592dfff41ef3c1a9d1306aaf3f00f
Binaryleo <leomouracss@fastmail.com>
2022-02-12 22:35:21 -0300

    add post method

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express, { Request, Response, NextFunction } from 'express'
 import usersRoute from './routes/users_route'
-const app = express()
+const app = express();
+
+// app config  <-------------------------
+app.use(express.json()) //add a middleware to check the content-type
+app.use(express.urlencoded({ extended: true })) //encoder
+
+
 app.use(usersRoute);
+
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ foo: 'bar' })
-})
+});
+
 app.listen(3000, () => {
   console.log('App running on port 3000')
-})
+});
