a30f8ba4f24cf309044385133eb0c785750dd6c5
Binaryleo <leomouracss@fastmail.com>
2022-02-12 22:35:18 -0300

    add post method

--- src/routes/users_route.ts
+++ src/routes/users_route.ts
@@ -5,11 +5,16 @@ const usersRoute = Router();
 usersRoute.get('/users',(req: Request, res: Response, next: NextFunction) => {
 const users = [{username: 'BinaryLeo'}];
 res.status(StatusCodes.OK).json({users});
-})
+});
 
 usersRoute.get('/users/:uuid',(req: Request<{uuid:string}>, res: Response, next: NextFunction) => {
 const uuid = req.params.uuid;
 res.status(StatusCodes.OK).send({uuid});
+});
+usersRoute.post('/users',(req: Request<{uuid:string}>, res: Response, next: NextFunction)=>{
+    const newuser = req.body;
+console.log(req.body);
+res.status(StatusCodes.CREATED).send({newuser});
 });
 
 export default usersRoute;
