0b814f606363094878234edc61b42e99f398d6ab
Binaryleo <leomouracss@fastmail.com>
2022-02-13 09:25:56 -0300

    CRUD implementation

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,19 +1,59 @@
 import User from '../models/user.model'
 import db from '../routes/db'
+
 class UserRepository {
   async findAllUsers(): Promise<User[]> {
     const query = `SELECT uuid, username  FROM application_user`
-   
-    const {rows} = await db.query<User>(query)
-    return rows || []; //return rows or empty array
+
+    const { rows } = await db.query<User>(query)
+    return rows || [] //return rows or empty array
   }
 
-  async findById(uuid:string): Promise<User>{
-      const query =`SELECT uuid, username  FROM application_user where uuid = $1`   ;
-      const values =[uuid];
-      const {rows} = await db.query<User>(query,values) ;
-      const [user] = rows; // user = rows[0]
-      return user;
+  async findById(uuid: string): Promise<User> {
+    const query = `SELECT uuid, username  FROM application_user where uuid = $1`
+    const values = [uuid]
+    const { rows } = await db.query<User>(query, values)
+    const [user] = rows // user = rows[0]
+    return user
+  }
+  async create(user: User): Promise<string> {
+    const script = `
+        INSERT INTO application_user (
+            username,
+            password
+        )
+        VALUES ($1, crypt($2, 'my_salt'))
+        RETURNING uuid
+    `
+
+    const values = [user.username, user.password]
+
+    const { rows } = await db.query<{ uuid: string }>(script, values)
+    const [newUser] = rows
+    return newUser.uuid
+  }
+
+  async update(user: User): Promise<void> {
+    const script = `
+        UPDATE application_user 
+        SET 
+            username = $1,
+            password = crypt($2, 'my_salt')
+        WHERE uuid = $3
+    `
+
+    const values = [user.username, user.password, user.uuid]
+    await db.query(script, values)
+  }
+
+  async remove(uuid: string): Promise<void> {
+    const crypt = `
+        DELETE
+        FROM application_user
+        WHERE uuid = $1
+    `
+    const values = [uuid]
+    await db.query(crypt, values)
   }
 }
 export default new UserRepository()
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -23,23 +23,30 @@ res.status(StatusCodes.OK).send({user}); // return the param
 //Post
 
 //------------------------------------> {req type - str}
-usersRoute.post('/users',(req: Request<{uuid:string}>, res: Response, next: NextFunction)=>{
-    const newuser = req.body;
-console.log(req.body);
-res.status(StatusCodes.CREATED).send({newuser});
+usersRoute.post('/users', async(req: Request<{uuid:string}>, res: Response, next: NextFunction)=>{
+const newuser = req.body;
+const uuid = await userRepository.create(newuser);
+res.status(StatusCodes.CREATED).send({uuid});
 });
 
 //Put
-usersRoute.put('/users/:uuid',(req: Request<{uuid:string}>, res: Response, next: NextFunction)=>{
-const uuid = req.params.uuid;// get the param from request
-const modifiedUser = req.body;
-modifiedUser.uuid = uuid;
-res.status(StatusCodes.OK).send(modifiedUser);
+usersRoute.put('/users/:uuid', async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    const modifiedUser = req.body;
+
+    modifiedUser.uuid = uuid;
+
+    await userRepository.update(modifiedUser);
+
+    res.status(StatusCodes.OK).send();
 });
 
 //delete
-usersRoute.delete('/users/:uuid',(req: Request<{uuid:string}>, res: Response, next: NextFunction)=>{
-    res.sendStatus(StatusCodes.OK)
-    });
+usersRoute.delete('/users/:uuid', async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    await userRepository.remove(uuid);
+    res.sendStatus(StatusCodes.OK);
+});
+
 
 export default usersRoute;
