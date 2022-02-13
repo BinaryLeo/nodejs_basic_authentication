ac39d4b3429684132e5ab1c5a97cb9e30537910c
Binaryleo <leomouracss@fastmail.com>
2022-02-12 22:16:46 -0300

    get by userId

--- src/routes/users_route.ts
+++ src/routes/users_route.ts
@@ -7,5 +7,9 @@ const users = [{username: 'BinaryLeo'}];
 res.status(200).json({users});
 })
 
+usersRoute.get('/users/:uuid',(req: Request<{uuid:string}>, res: Response, next: NextFunction) => {
+const uuid = req.params.uuid;
+res.status(200).send({uuid});
+});
 
 export default usersRoute;
