e23116d5b0d15f0b71bf11ce0a770b3379d50425
Binaryleo <leomouracss@fastmail.com>
2022-02-26 20:28:31 -0300

    extend express and  auth. middleware

---
+++ src/middlewares/basic-authentication.middleware.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from 'express'
+import ForbiddenError from '../models/errors/forbidden.error'
+import userRepository from '../repositories/user.repository'
+async function basicAuthenticationMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const authorizationHeader = req.headers['authorization']
+    if (!authorizationHeader) {
+      throw new ForbiddenError('Ivallid credentials.')
+    }
+    const [authenticationType, token] = authorizationHeader.split(' ')
+    if (authenticationType !== 'Basic' || !token) {
+      throw new ForbiddenError('Invalid authentication type')
+    }
+    const tokenContent = Buffer.from(token, 'base64').toString('utf-8')
+    const [username, password] = tokenContent.split(':');
+    if (!username || !password) {
+      throw new ForbiddenError('Credentials not provided.');
+    }
+    const user = await userRepository.findByUsernameAndPassword(
+      username,
+      password,
+    )
+
+    //--------------------
+    console.log(user);
+    console.log(tokenContent);
+    //--------------------
+    if (!user) {
+      throw new ForbiddenError('Username or password invalid')
+    }
+    req.user = user;
+    next() //Keeps propagating the request between middleware and routes
+  } catch (error) {
+    next(error);
+  }
+}
+export default basicAuthenticationMiddleware
--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -1,38 +1,29 @@
 import { Request, Response, NextFunction, Router } from 'express'
-import ForbiddenError from '../models/errors/forbidden.error'
-import userRepository from '../repositories/user.repository'
 
-const authorizationRoute = Router()
+import JWT from 'jsonwebtoken'
+import { StatusCodes } from 'http-status-codes'
+import basicAuthenticationMiddleware from '../middlewares/basic-authentication.middleware'
+import ForbiddenError from '../models/errors/forbidden.error'
+const authorizationRoute = Router();
 
 authorizationRoute.post(
   '/token',
+  basicAuthenticationMiddleware,
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const authorizationHeader = req.headers['authorization']
-      if (!authorizationHeader) {
-        throw new ForbiddenError('Ivallid credentials.')
+      const user = req.user;
+      if (!user) {
+        throw new ForbiddenError('User not informed');
       }
-      const [authenticationType, token] = authorizationHeader.split(' ')
-      if (authenticationType !== 'Basic' || !token) {
-        throw new ForbiddenError('Invalid authentication type');
-      }
-      const tokenContent = Buffer.from(token, 'base64').toString('utf-8');
-      const [username, password] = tokenContent.split(':');
-      if (!username || !password) {
-        throw new ForbiddenError('Credentials not provided.');
-      }
-      const user = await userRepository.findByUsernameAndPassword(username, password);
-
-      //-------------------- 
-      console.log(user);
-      console.log(tokenContent);
-      //-------------------- 
-
-
+      const jwtPayload = { username: user.username };
+      const jwtOptions = { subject: user?.uuid };
+      const jwtSecret = 'scretKey';
+      const jwt = JWT.sign(jwtPayload, jwtSecret, jwtOptions);
+      res.status(StatusCodes.OK).json({ token: jwt });
     } catch (error) {
       next(error);
     }
   },
 )
 
-export default authorizationRoute
+export default authorizationRoute;
