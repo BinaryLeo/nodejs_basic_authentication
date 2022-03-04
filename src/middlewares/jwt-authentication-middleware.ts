68411a8be98a795c9d7bc98e85f59d93a8939480
Binaryleo <leomouracss@fastmail.com>
2022-03-03 21:50:59 -0300

    refactoring

--- src/middlewares/jwt-authentication-middleware.ts
+++ src/middlewares/jwt-authentication-middleware.ts
@@ -1,34 +1,34 @@
-import { Request, Response, NextFunction } from 'express'
-import ForbiddenError from '../models/errors/forbidden.error'
-import JWT from 'jsonwebtoken'
+import { Request, Response, NextFunction } from 'express';
+import ForbiddenError from '../models/errors/forbidden.error';
+import JWT from 'jsonwebtoken';
 async function jwtAuthenticationMiddleware(
   req: Request,
   res: Response,
   next: NextFunction,
 ) {
   try {
-    const authorizationHeader = req.headers['authorization']
+    const authorizationHeader = req.headers['authorization'];
     if (!authorizationHeader) {
-      throw new ForbiddenError('Credentials not provided.')
+      throw new ForbiddenError('Credentials not provided.');
     }
-    const [authenticationType, token] = authorizationHeader.split(' ')
+    const [authenticationType, token] = authorizationHeader.split(' ');
     if (authenticationType !== 'Bearer' || !token) {
-      throw new ForbiddenError('Invalid authentication type')
+      throw new ForbiddenError('Invalid authentication type');
     }
     try {
-      const tokenPayload = JWT.verify(token, 'scretKey')
+      const tokenPayload = JWT.verify(token, 'scretKey');
 
       if (typeof tokenPayload !== 'object' || !tokenPayload.sub) {
-        throw new ForbiddenError('Invalid token')
+        throw new ForbiddenError('Invalid token');
       }
-      const user = { uuid: tokenPayload.sub, username: tokenPayload.username }
-      req.user = user
-      next()
+      const user = { uuid: tokenPayload.sub, username: tokenPayload.username };
+      req.user = user;
+      next();
     } catch (error) {
-      throw new ForbiddenError('Invalid token')
+      throw new ForbiddenError('Invalid token');
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
-export default jwtAuthenticationMiddleware
+export default jwtAuthenticationMiddleware;
