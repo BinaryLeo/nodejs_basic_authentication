4cf3c97a5d2f526af516f409b862885fecf1be99
Binaryleo <leomouracss@fastmail.com>
2022-03-04 21:07:51 -0300

    Refresh Token

--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -26,6 +26,29 @@ authorizationRoute.post(
   }
 );
 
+
+authorizationRoute.post(
+  '/token/refresh',
+  jwtAuthenticationMiddleware,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const user = req.user;
+      if (!user) {
+        throw new ForbiddenError('User not informed');
+      }
+      const jwtPayload = { username: user.username };
+      const jwtOptions: SignOptions = { subject: user?.uuid, expiresIn: '1m' }; //15 minutes
+      const jwtSecret = 'scretKey';
+      const jwt = JWT.sign(jwtPayload, jwtSecret, jwtOptions);
+      res.status(StatusCodes.OK).json({ token: jwt });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+ 
+
+
 // Already validated by jwt Authentication Middleware, do we need to call the endpoint to respond OK
 authorizationRoute.post('/token/validate', jwtAuthenticationMiddleware,(req: Request, res: Response, next: NextFunction) => {
   res.sendStatus(StatusCodes.OK);
